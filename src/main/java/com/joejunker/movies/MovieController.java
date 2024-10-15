9f9f3ba7c65b23c0d1d6d2ab5fae76b8c5ff8eac
Joe1Jun <junker91joe@gmail.com>
2024-10-15 10:52:03 +0200

    Created movie repository interface that extends the mongoRepository. Created a service class for movie

--- src/main/java/com/joejunker/movies/MovieController.java
+++ src/main/java/com/joejunker/movies/MovieController.java
@@ -6,13 +6,19 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+//This annotation is used to tell Spring Boot that the class is a RESTful controller. It indicates that this class
+//will handle HTTP requests and responses in a REST API.
 @RestController
+//This annotation maps HTTP requests to specific methods or classes. In this case, it's mapping all routes that start with /api/v1/movies to this controller. Any endpoint 
+//defined inside this class will have /api/v1/movies as the base URL.
 @RequestMapping("/api/v1/movies")
 
 public class MovieController {
 	
 	@GetMapping
-	public ResponseEntity<String> allMovies () {
+	//ResponseEntity is used to have control over the HTTP response body 
+	//and status code, similar to how you handle responses in Node.js with res.status().send().
+      public ResponseEntity<String> getAllMovies () {
 		
 		return new ResponseEntity<String>("All Movies", HttpStatus.OK);
 		
---
+++ src/main/java/com/joejunker/movies/MovieService.java
@@ -0,0 +1,16 @@
+package com.joejunker.movies;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class MovieService {
+	
+	private MovieRepository movieRepository;
+	
+	public List<Movie> allMovies() {
+		
+		
+		
+	}
+
+}
