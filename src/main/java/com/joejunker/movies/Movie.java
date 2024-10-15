77057591bceaaef3b3c9f652a37fad36029ad8cf
Joe1Jun <junker91joe@gmail.com>
2024-10-15 10:27:46 +0200

    Created basic rest controller for movies collection

--- src/main/java/com/joejunker/movies/Movie.java
+++ src/main/java/com/joejunker/movies/Movie.java
@@ -4,8 +4,23 @@ import java.util.List;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.DocumentReference;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+// This is the same as using @Entity for relational databases
+// We have to clarify the collection name here as with noSql there is no table
+// automatically created from the class object just a collection object 
 @Document(collection = "movies")
+//This generated all getters and setters for all non final instance variables
+// Also creates toString method hashcode and equals methods for all instance variables
+@Data
+//@AllArgsConstructor: Generates a constructor like public Movie(String id, String title, String director).
+@AllArgsConstructor
+//@NoArgsConstructor: Generates a no-argument constructor like public Movie().
+@NoArgsConstructor
 public class Movie {
 	
 	@Id
@@ -19,6 +34,13 @@ public class Movie {
 	private String poster;
 	private List<String> genres;
 	private List <String> backdrops;
+	
+	// This annotation will create ids or refernces to the reviews and the reviews will be 
+	// stored in a separate collection
+	@DocumentReference
+	private List<Review> reviewIds;
+	
+	
 	
 
 }
--- src/main/java/com/joejunker/movies/Review.java
+++ src/main/java/com/joejunker/movies/Review.java
@@ -1,5 +1,27 @@
 package com.joejunker.movies;
 
-public class Review {
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+// Specifies this class as a collection in the Mongo database
+@Document(collection = "reviews")
+//Lombok generates methods toString, hashcode, equals getters and setters.
+@Data
+//@AllArgsConstructor: Generates a constructor like public Movie(String id, String title, String director).
+@AllArgsConstructor
+//@NoArgsConstructor: Generates a no-argument constructor like public Movie().
+@NoArgsConstructor
+public class Review {
+   
+	
+	@Id
+	private ObjectId id;
+	
+	
 }
