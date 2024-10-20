7889d2e27d94dc61bfb3f27150866bef0491a48f
Joe1Jun <junker91joe@gmail.com>
2024-10-20 12:15:43 +0200

    Passed movies retreived from database down to hero component as a prop and successfully displayed movie items in a carousel on the home page

--- MovieClient/movie-gold-v1/src/Components/hero/Hero.jsx
+++ MovieClient/movie-gold-v1/src/Components/hero/Hero.jsx
@@ -1,11 +1,43 @@
 import React from "react";
 
+import Carousel from "react-material-ui-carousel";
+import { Paper} from  "@mui/material"
 
 
-const Hero = () =>{
+const Hero = ({movies}) =>{
 
 return (
+  <div className="movie-carousel-container">
+     <Carousel>
+      {
+        movies?.map((movie) => {
+           return(
+            <Paper>
+                <div className="movie-card-container">
+                      <div className="movie-card">
+                        <div className="movie-detail">
+                            <div className="movie-poster">
+                                <img src={movie.poster} alt="" />
+                            </div>
+                            <div className="move-title">
+                                <h4>{movie.title}</h4>
+                            </div>
+                        </div>
+                      </div>
+                </div>
+            </Paper>
+           )
 
+        })
+
+
+
+
+      }
+
+     </Carousel>
+
+  </div>
 
 
 )
--- MovieClient/movie-gold-v1/src/Components/home/Home.jsx
+++ MovieClient/movie-gold-v1/src/Components/home/Home.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-
+import Hero from "../hero/Hero";
 
 const Home = ({movies}) => {
 
@@ -7,7 +7,7 @@ const Home = ({movies}) => {
  return (
     <div>
 
-        Welcome !!!
+      <Hero movies={movies} />
     </div>
 
 
