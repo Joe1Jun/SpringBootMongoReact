525b388e2dffd6438bb514ec9a10f0bd3a0127e2
Joe1Jun <junker91joe@gmail.com>
2024-10-22 13:32:39 +0200

    Created Server folder

--- Server/src/main/java/com/joejunker/movies/MovieController.java
+++ Server/src/main/java/com/joejunker/movies/MovieController.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 /// This annotation marks the class as a Spring REST controller.
 //It allows the class to handle HTTP requests and return responses.
 @RestController
+@CrossOrigin(origins = "http://localhost:5173")
 @RequestMapping("/api/v1/movies") // This specifies the base URI for all requests handled by this controller.
 public class MovieController {
 
